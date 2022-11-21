308a3fd29be7787017c2e6eebe63ff1231e3c194
Not TCA <realtcatech@gmail.com>
2022-11-21 20:38:51 +0800

    add shard id to status

--- src/events/shardReady.ts
+++ src/events/shardReady.ts
@@ -1,6 +1,12 @@
+import { ActivityType } from "discord.js";
 import { Event } from "../structures/Event";
 import logger from "../utils/logger";
 
 export default new Event("shardReady", (client, id) => {
   logger.info(`Launched shard #${id}.`);
+
+  client.user.setActivity(`my toaster | Shard ${id}`, {
+    shardId: id,
+    type: ActivityType.Watching,
+  });
 });
